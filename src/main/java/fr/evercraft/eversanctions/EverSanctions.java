e3bd5f3267f0b41dc3ca46d890c639a2a02c964e
lesbleu <lesbleu@evercraft.fr>
2016-07-16 03:14:53 +0200

    Fail copier/coller

--- src/main/java/fr/evercraft/eversanctions/EverSanctions.java
+++ src/main/java/fr/evercraft/eversanctions/EverSanctions.java
@@ -1,3 +1,19 @@
+/*
+ * This file is part of EverSanctions.
+ *
+ * EverSanctions is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EverSanctions is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EverSanctions.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.evercraft.eversanctions;
 
 import org.spongepowered.api.plugin.Dependency;
@@ -6,10 +22,10 @@ import org.spongepowered.api.plugin.Plugin;
 import fr.evercraft.everapi.plugin.EPlugin;
 import fr.evercraft.eversanctions.command.sub.ESReload;
 
-@Plugin(id = "fr.evercraft.everkits", 
-		name = "EverKits", 
+@Plugin(id = "fr.evercraft.eversanctions", 
+		name = "EverSanctions", 
 		version = "1.2", 
-		description = "Manage Kit",
+		description = "Manage Sanctions",
 		url = "http://evercraft.fr/",
 		authors = {"rexbut"},
 		dependencies = {
